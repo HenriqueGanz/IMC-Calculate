86cb4b4c650b4fb50419778b46d1a265be3962a2
Henrique Ganz <113484203+HenriqueGanz@users.noreply.github.com>
2022-10-31 19:56:03 -0300

    Delete Calculadora IMC 02 directory

--- Calculadora IMC 02/js/error.js
+++
@@ -1,9 +0,0 @@
-export const alertError = {
-    element: document.querySelector(".error"),
-    open() {
-        alertError.element.classList.remove("hide")
-    },
-    close() {
-        alertError.element.classList.add("hide")
-    }
-}
